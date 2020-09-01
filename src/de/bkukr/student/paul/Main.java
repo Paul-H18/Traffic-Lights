818a1be91a5f35ab373bdb9ac67f009734b4caf9
Paul-H18 <paul.hornig@gmx.de>
2020-09-01 10:01:45 +0200

    x

--- src/de/bkukr/student/paul/Main.java
+++ src/de/bkukr/student/paul/Main.java
@@ -3,11 +3,15 @@ package de.bkukr.student.paul;
 public class Main {
 
     public static void main(String[] args) {
+
 	    TrafficLight tl = new TrafficLight(45);
+	    TrafficLight tl2 = new TrafficLight(46, tl);
 
         tl.printState();
 
         tl.shiftRed(1);
+
+        System.out.println(tl2.prevGreen());
 
 
     }
--- src/de/bkukr/student/paul/TrafficLight.java
+++ src/de/bkukr/student/paul/TrafficLight.java
@@ -121,8 +121,6 @@ public class TrafficLight {
 
 
 
-
-
 }
 
 
